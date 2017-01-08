f759a8353cb9f534b68ec38651c66a6dea8ed798
romjerome <romjerome@users.noreply.github.com>
2017-01-08 11:42:45 +0100

    GEP041: include siblings of the active person

--- gramps/gui/selectors/selectperson.py
+++ gramps/gui/selectors/selectperson.py
@@ -111,6 +111,11 @@ class SelectPerson(BaseSelector):
                     mother = dbstate.db.get_person_from_handle(mother_handle)
                     mid = mother.get_gramps_id()
                     sfilter.add_rule(rules.person.HasIdOf([mid]))
+                sib_list = family.get_child_ref_list()
+                for sib_ref in sib_list:
+                    sibling = dbstate.db.get_person_from_handle(sib_ref.ref)
+                    sbid = sibling.get_gramps_id()
+                    sfilter.add_rule(rules.person.HasIdOf([sbid]))
             families = len(active_person.get_family_handle_list())
             if families != 0:
                 for family_handle in active_person.get_family_handle_list():
